ce85fe3ab5927608273587a2be7782358b1909ed
Nic <nic@askable.com>
2021-12-07 16:35:21 +1000

    nav css changes

--- src/components/PrimaryNav.js
+++ src/components/PrimaryNav.js
@@ -9,10 +9,10 @@ const PrimaryNav = () => (
   <Navbar expand="md" bg="dark" variant="dark" id="primary-navbar">
     <Container fluid="lg">
       <Navbar.Toggle aria-controls="primary-navbar-collapse" />
-      <div className="search-wrapper ms-auto ps-1 w-auto flex-grow-1">
+      <div className="search-wrapper ms-auto w-auto flex-grow-1">
         <FormControl type="text" placeholder="Search..." aria-label="Search" variant="outline-light" />
       </div>
-      <Navbar.Collapse id="primary-navbar-collapse" className="flex-grow-0">
+      <Navbar.Collapse id="primary-navbar-collapse" className="flex-grow-0 me-1">
         <Nav className="me-0">
           {routes.map(({ path, label }) => <Nav.Link href={path} key={label}>{label}</Nav.Link>)}
         </Nav>
