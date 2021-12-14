57f162c604d57c63823d911a36ecb8a557cad482
Nic <nic@askable.com>
2021-12-15 08:43:09 +1000

    rearramge dependencies

--- workflow/build-dir-indexes.js
+++ workflow/build-dir-indexes.js
@@ -1,15 +1,15 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.readdirSync(path.join(__dirname, '../build'))
-.forEach((filename) => {
-    const match = filename && filename.match && filename.match(/^(?!index|200)(.+)\.html/);
-    if (!(match && match[1])) return;
+// fs.readdirSync(path.join(__dirname, '../build'))
+// .forEach((filename) => {
+//     const match = filename && filename.match && filename.match(/^(?!index|200)(.+)\.html/);
+//     if (!(match && match[1])) return;
 
-    const dir = path.join(__dirname, '../build', match[1]);
+//     const dir = path.join(__dirname, '../build', match[1]);
 
-    if (!fs.existsSync(dir)) fs.mkdirSync(dir);
+//     if (!fs.existsSync(dir)) fs.mkdirSync(dir);
 
-    fs.copyFileSync(`${dir}.html`, `${dir}/index.html`);
-});
+//     fs.renameSync(`${dir}.html`, `${dir}/index.html`);
+// });
 
